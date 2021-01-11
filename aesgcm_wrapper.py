9795c029f1e1d0c2c5aa69d9676dbe8aae5468f2
Luigi Rocca <luigi@sorry-not-my-email.no>
2021-01-11 19:58:22 +0100

    encoding a json request to a json response, according to spec

--- aesgcm_wrapper.py
+++ aesgcm_wrapper.py
@@ -7,14 +7,14 @@ import unittest
 def encode_raw_bytes(input_bytes):
     key = get_random_bytes(16)
     cipher = AES.new(key, AES.MODE_GCM)
-    ciphertext, tag = cipher.encrypt_and_digest(input_bytes)
+    ciphertext, mac = cipher.encrypt_and_digest(input_bytes)
     nonce = cipher.nonce
-    return key, ciphertext, tag, nonce
+    return key, ciphertext, mac, nonce
 
-def decode_raw_bytes(key, ciphertext, tag, nonce):
+def decode_raw_bytes(key, ciphertext, mac, nonce):
     cipher = AES.new(key, AES.MODE_GCM, nonce)
     try:
-        decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
+        decrypted_data = cipher.decrypt_and_verify(ciphertext, mac)
         return decrypted_data
     except:
         return None
@@ -33,23 +33,33 @@ def decode_raw_bytes(key, ciphertext, tag, nonce):
 #     nonce: "<base64-encoded-nonce>"
 # }
 def encode_json(json_request):
-    json_response = '{"data":"TWFu","key":"TWFu","mac":"TWFu","nonce":"TWFu"}'
+    request = json.loads(json_request)
+    key, ciphertext, mac, nonce = encode_raw_bytes(request['data'].encode("utf-8"))
+    response = {
+        "data": base64.b64encode(ciphertext).decode("utf-8"),
+        "key": base64.b64encode(key).decode("utf-8"),
+        "mac": base64.b64encode(mac).decode("utf-8"),
+        "nonce": base64.b64encode(nonce).decode("utf-8"),
+    }
+
+    json_response = json.dumps(response)
     return json_response
     
+
 
 class TestWrapper(unittest.TestCase):
 
     clear_text = "foo bar azang zving"
 
     def test_raw_correct(self):
-        key, ciphertext, tag, nonce = encode_raw_bytes(self.clear_text.encode("utf-8"))
-        decoded_text = decode_raw_bytes(key, ciphertext, tag, nonce).decode("utf-8")
+        key, ciphertext, mac, nonce = encode_raw_bytes(self.clear_text.encode("utf-8"))
+        decoded_text = decode_raw_bytes(key, ciphertext, mac, nonce).decode("utf-8")
         self.assertEqual(self.clear_text, decoded_text)
 
     def test_raw_wrong(self):
-        key, ciphertext, tag, nonce = encode_raw_bytes(self.clear_text.encode("utf-8"))
+        key, ciphertext, mac, nonce = encode_raw_bytes(self.clear_text.encode("utf-8"))
         wrong_key = get_random_bytes(16)
-        decoded_text = decode_raw_bytes(wrong_key, ciphertext, tag, nonce)
+        decoded_text = decode_raw_bytes(wrong_key, ciphertext, mac, nonce)
         self.assertIsNone(decoded_text)
 
     def test_json(self):
@@ -59,7 +69,7 @@ class TestWrapper(unittest.TestCase):
         result = json.loads(json_response)
 
         for k, v in result.items ():
-            result[k] = base64.b64decode(v)
+            result[k] = base64.b64decode(v.encode("utf-8"))
 
         decoded_text = decode_raw_bytes(result['key'], result['data'], result['mac'], result['nonce']).decode("utf-8")
         self.assertEqual(self.clear_text, decoded_text)
